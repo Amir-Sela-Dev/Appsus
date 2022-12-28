fe74160fed6ac31d51f02c09aa97fcbfe05f5b63
Amir Sela <amirsela10@gmail.com>
2022-12-28 11:28:13 +0200

    first commit

---
+++ views/home.jsx
@@ -0,0 +1,14 @@
+
+export function Home() {
+
+    return <section className="home">
+        <div className="hero">
+            <h1>Welcome to home page!</h1>
+        </div>
+        <div className="home-nav-container">
+            <div className="mail"></div>
+            <div className="notes"></div>
+            <div className="book-shop"></div>
+        </div>
+    </section>
+}
