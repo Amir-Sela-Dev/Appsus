008d629233f33d696b7811073e65bea813600d91
Amir Yakubov <yakobub@gmail.com>
2022-12-31 15:42:35 +0200

    add icons to main nav, change the home page, style the open menu

--- views/home.jsx
+++ views/home.jsx
@@ -1,14 +1,18 @@
-
+const { Link } = ReactRouterDOM
 export function Home() {
 
     return <section className="home">
-        <div className="hero">
-            <h1>Welcome to home page!</h1>
-        </div>
+
+        {/* <h1>Welcome to home page!</h1> */}
+
         <div className="home-nav-container">
-            <div className="mail"></div>
-            <div className="notes"></div>
-            <div className="book-shop"></div>
+            <Link to="/mail">
+                <div className="mail"></div>
+            </Link>
+            <Link to="/note">
+                <div className="notes"></div>
+            </Link>
+            {/* <div className="book-shop"></div> */}
         </div>
     </section>
 }
