cd37f7ed37619000d5fd31927a89e26172bf3c8d
Amir Sela <amirsela10@gmail.com>
2022-12-31 19:01:35 +0200

    first home page design

--- views/home.jsx
+++ views/home.jsx
@@ -2,8 +2,9 @@ const { Link } = ReactRouterDOM
 export function Home() {
 
     return <section className="home">
-
-        {/* <h1>Welcome to home page!</h1> */}
+        {/* <div className="title flex"><h1>Appsus</h1></div> */}
+        <div className="google-img"> <div className="home-row1">  <h1>The app that help you orgenize your life.</h1> <img src="/assets/img/google-assistant.svg" alt="" /></div>
+            <div className="hero-img"> </div> </div>
 
         <div className="home-nav-container">
             <Link to="/mail">
@@ -12,7 +13,7 @@ export function Home() {
             <Link to="/note">
                 <div className="notes"></div>
             </Link>
-            {/* <div className="book-shop"></div> */}
+            <div className="book-shop"></div>
         </div>
     </section>
 }
