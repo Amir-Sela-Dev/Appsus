93257da5af5dbb85c5d4be23b27e8eecc2990a6d
Amir Sela <amirsela10@gmail.com>
2022-12-31 20:00:06 +0200

    home page done

--- views/home.jsx
+++ views/home.jsx
@@ -1,19 +1,29 @@
-const { Link } = ReactRouterDOM
+const { Link, useNavigate } = ReactRouterDOM
 export function Home() {
+    const navigate = useNavigate()
 
     return <section className="home">
         {/* <div className="title flex"><h1>Appsus</h1></div> */}
-        <div className="google-img"> <div className="home-row1">  <h1>The app that help you orgenize your life.</h1> <img src="/assets/img/google-assistant.svg" alt="" /></div>
-            <div className="hero-img"> </div> </div>
-
-        <div className="home-nav-container">
-            <Link to="/mail">
-                <div className="mail"></div>
-            </Link>
-            <Link to="/note">
-                <div className="notes"></div>
-            </Link>
-            <div className="book-shop"></div>
+        <div className="home-hero"> <div className="home-row1">  <h1>The app that help you orgenize your life.</h1> <div className="google-assistent"><img src="/assets/img/google-assistant.svg" alt="" /></div></div>
+            <div className="home-nav-container">
+                <div className="home-mail-container flex">
+                    <Link to="/mail">
+                        <div className="mail"></div>
+                    </Link>
+                    <button onClick={() => { navigate("/mail") }}>Appsus mail</button>
+                </div>
+                <div className="home-note-container flex">
+                    <Link to="/note">
+                        <div className="notes"></div>
+                    </Link>
+                    <button onClick={() => { navigate("/note") }}>Appsus notes</button>
+                </div>
+                <div className="home-book-container flex">
+                    <div className="book-shop"></div>
+                    <button>Appsus book-shop</button>
+                </div>
+            </div>
         </div>
+
     </section>
 }
