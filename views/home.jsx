069dc2962bd889f5d714fa2870305b8aaef2f6f9
Amir Yakubov <yakobub@gmail.com>
2023-01-01 02:19:07 +0200

    add animations and about

--- views/home.jsx
+++ views/home.jsx
@@ -4,8 +4,16 @@ export function Home() {
 
     return <section className="home">
         {/* <div className="title flex"><h1>Appsus</h1></div> */}
-        <div className="home-hero"> <div className="home-row1">  <h1>The app that help you orgenize your life.</h1> <div className="google-assistent"><img src="/assets/img/google-assistant.svg" alt="" /></div></div>
-            <div className="home-nav-container">
+        <div className="home-hero">
+
+            <div className="home-row1">
+                <h1>The app that help you orgenize your life.</h1>
+
+                <div className="google-assistent animate__animated animate__fadeInRight">
+                    <img src="/assets/img/google-assistant.svg" alt="" /></div>
+            </div>
+
+            <div className="home-nav-container animate__animated animate__bounceInUp">
                 <div className="home-mail-container flex">
                     <Link to="/mail">
                         <div className="mail"></div>
